b7649d552f3632b5e6a0e9c27481418c054143e9
Joe Bowman <joe@ingenuity.build>
2022-06-16 02:51:16 +0100

    update scripts for rhapsody-5

--- rhapsody/vars.sh
+++ rhapsody/vars.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 DIR=~/rhapsody
-QS_VERSION=v0.2.0
+QS_VERSION=v0.3.0
 QS_HOME=$DIR/.quicksilver
 
 QS_BIN=$DIR/quicksilver/build/quicksilverd
 
-CHAIN_ID=rhapsody-4
+CHAIN_ID=rhapsody-5
 
 
