f1eb4012519fa0c3666b92704cbc3ed64edd4be1
Ankit <github@ankitmalik.in>
2022-06-17 13:24:00 +0400

    Use ${CHAIN_ID} var instead of hardcoding chain ID

--- rhapsody/balances.sh
+++ rhapsody/balances.sh
@@ -10,5 +10,5 @@ fi
 QS_KEY=$($QS_BIN --home $QS_HOME keys show validator --output=json | jq .address -r)
 
 echo "Balance of $QS_KEY on $CHAIN_ID:"
-$QS_BIN --home $QS_HOME q bank balances $QS_KEY --node http://seed.rhapsody-5.quicksilver.zone:26657 --output=json | jq .balances
+$QS_BIN --home $QS_HOME q bank balances $QS_KEY --node http://seed.${CHAIN_ID}.quicksilver.zone:26657 --output=json | jq .balances
 echo
